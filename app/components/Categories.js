5e8f85adf52e4847c2f1cbb9a4928a9cf7cf6bec
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-01 18:24:14 +0100

    add Header tabs

---
+++ app/components/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Categories = () => {
+    return (
+        <View>
+            <Text>
+                Categories
+            </Text>
+        </View>
+    )
+}
+
+export default Categories
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import HeaderTabs from '../components/HeaderTabs';
+import { localRestaurants } from '../data/localRestaurants';
 
 import { Text, View } from 'react-native'
 import Screen from '../components/Screen'
+
 const HomeScreen = () => {
+    const [activeTab, setActiveTab] = useState("Delivery");
     return (
-        <View>
-            <Text>iknowsaint</Text>
-        </View>
+        <Screen>
+            <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        </Screen>
     )
 }
 
