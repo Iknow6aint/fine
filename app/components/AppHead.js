60f3a05352a62401e81ce4909911434196c80319
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-02 00:09:29 +0100

    account screen

---
+++ app/components/AppHead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+const AppHead = () => {
+    return (
+        <View>
+            <Text>AppHead</Text>
+        </View>
+    )
+}
+
+export default AppHead
