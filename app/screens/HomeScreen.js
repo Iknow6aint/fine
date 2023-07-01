9319d3ae7b39268a564634c308222b62dadb69ce
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-01 22:47:21 +0100

    home page

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -5,14 +5,23 @@ import { localRestaurants } from '../data/localRestaurants';
 import { ScrollView, Alert, ActivityIndicator } from 'react-native';
 import Screen from '../components/Screen'
 import Categories from '../components/Categories';
+import colors from '../configs/colors'
+import tailwind from 'tailwind-react-native-classnames';
+import RestaurantItem from '../components/RestaurantItem';
+
+
 
 const HomeScreen = () => {
     const [activeTab, setActiveTab] = useState("Delivery");
+    const [restaurantData, setRestaurantData] = useState(localRestaurants)
+    const [loading, setLoading] = useState(false)
     return (
         <Screen>
             <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
             <ScrollView>
                 <Categories />
+                {loading && <ActivityIndicator size="large" color={colors.primary} style={tailwind`mt-2 mb-6`} />}
+                <RestaurantItem restaurantData={restaurantData} />
             </ScrollView>
         </Screen>
     )
