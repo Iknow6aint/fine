3de0cdb77e400ebe666a88442290f8deb191f25b
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-02 10:43:18 +0100

    forms

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import { View, Text } from 'react-native';
 import JoinScreen from '../screens/JoinScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +15,9 @@ const AuthNavigator = () => {
             }}
         >
             <Stack.Screen name="Join" component={JoinScreen} />
+            <Stack.Screen name="UserLogin" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+
         </Stack.Navigator>
     )
 }
---
+++ app/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+
+
+const SignupScreen = () => {
+    return (
+        <View>
+            <Text>Signup</Text>
+        </View>
+    )
+}
+
+export default SignupScreen
