74d9c353b78ba2fd06171923d64db61afce810c2
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-11 16:17:53 +0100

    app head

--- app/components/SuggestionItem.js
+++ app/components/SuggestionItem.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { View, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import tailwind from 'tailwind-react-native-classnames';
+const tw = tailwind
 
-function SuggestionItem() {
+
+function SuggestionItem({ breakfast, lunch, dinner }) {
     return (
-        <div>SuggestionItem</div>
+        <View>
+            {breakfast && (
+                <View style={tw`mt-8`}>
+                    <Text style={tw`text-lg font-bold`}>Breakfast:</Text>
+                    {breakfast.map((item, index) => (
+                        <Text key={index} style={tw`ml-2`}>
+                            {item.title} - ${item.price} Naira
+                        </Text>
+                    ))}
+                </View>
+            )}
+            {lunch && (
+                <View style={tw`mt-8`}>
+                    <Text style={tw`text-lg font-bold`}>Lunch:</Text>
+                    {lunch.map((item, index) => (
+                        <Text key={index} style={tw`ml-2`}>
+                            {item.title} - {item.price} Naira
+                        </Text>
+                    ))}
+                </View>
+            )}
+            {dinner && (
+                <View style={tw`mt-8`}>
+                    <Text style={tw`text-lg font-bold`}>Dinner:</Text>
+                    {dinner.map((item, index) => (
+                        <Text key={index} style={tw`ml-2`}>
+                            {item.title} - {item.price} Naira
+                        </Text>
+                    ))}
+                </View>
+            )}
+        </View>
     )
 }
 
--- app/screens/Suggestion.js
+++ app/screens/Suggestion.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 import { foods } from "../data/foodsData";
 import colors from '../configs/colors';
 import tailwind from 'tailwind-react-native-classnames';
 import Screen from '../components/Screen'
+import SuggestionItem from "../components/SuggestionItem";
 const tw = tailwind;
 
 
 
-const Suggestion = () => {
+const Suggestion = ({ navigation }) => {
     const [budget, setBudget] = useState('');
     const [breakfast, setBreakfast] = useState(null);
     const [lunch, setLunch] = useState(null);
@@ -32,51 +34,24 @@ const Suggestion = () => {
         setBreakfast(breakfastItems.slice(0, 3));
         setLunch(lunchItems.slice(0, 3));
         setDinner(dinnerItems.slice(0, 3));
-
         console.log(breakfast, lunch, dinner);
 
     };
 
     return (
         <View style={tw`flex-1 justify-center items-center bg-white`}>
+            <TouchableOpacity style={tailwind`absolute top-9 left-4 z-30 w-9 h-9 rounded-full bg-white justify-center items-center shadow`} onPress={() => navigation.goBack()}>
+                <Ionicons name="arrow-back" size={18} color={colors.black} />
+            </TouchableOpacity>
             <Text style={tw`text-2xl mb-4`}>Budget App</Text>
             <TextInput
                 style={tw`border border-gray-300 rounded w-64 px-4 py-2 mb-4`}
                 placeholder="Enter your budget"
                 onChangeText={handleBudgetInput}
-                keyboardType="numeric"
             />
-            <Button title="Filter" onPress={handleFilter} />
-            {breakfast && (
-                <View style={tw`mt-8`}>
-                    <Text style={tw`text-lg font-bold`}>Breakfast:</Text>
-                    {breakfast.map((item, index) => (
-                        <Text key={index} style={tw`ml-2`}>
-                            {item.title} - ${item.price} Naira
-                        </Text>
-                    ))}
-                </View>
-            )}
-            {lunch && (
-                <View style={tw`mt-8`}>
-                    <Text style={tw`text-lg font-bold`}>Lunch:</Text>
-                    {lunch.map((item, index) => (
-                        <Text key={index} style={tw`ml-2`}>
-                            {item.title} - {item.price} Naira
-                        </Text>
-                    ))}
-                </View>
-            )}
-            {dinner && (
-                <View style={tw`mt-8`}>
-                    <Text style={tw`text-lg font-bold`}>Dinner:</Text>
-                    {dinner.map((item, index) => (
-                        <Text key={index} style={tw`ml-2`}>
-                            {item.title} - {item.price} Naira
-                        </Text>
-                    ))}
-                </View>
-            )}
+            <Button title="Get Recommendation" onPress={handleFilter} >
+                <SuggestionItem breakfast={breakfast} lunch={lunch} dinner={dinner} />
+            </Button>
         </View>
     )
 }
